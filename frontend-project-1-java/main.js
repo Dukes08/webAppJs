e09e4be0aa3011c0b5e5b7bc49068e7c363d7f44
Macbook Pro <hugo.duque@correo.unimet.edu.ve>
2023-10-01 21:24:35 -0400

    Segunda fusion con Js

--- frontend-project-1-java/main.js
+++ frontend-project-1-java/main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopM = document.querySelector('.desktop-menu');
+const hamMenuIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopM.classList.toggle('inactive'); /*con classlist accedemos a las clases que tiene, con toggle 
     logramos que active o desactive inactive*/
+}
+
+hamMenuIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
